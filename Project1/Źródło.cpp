2b7fdfe5e0854000bc3b080c03bb7a6fa909f9a8
Puczyński Mirosław <m_puczynski@ORCA.LOCAL>
2019-10-12 15:00:15 +0200

    end

--- Project1/Źródło.cpp
+++ Project1/Źródło.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
-
+#include<iomanip>
 using namespace std;
 int main() {
 	cout << "podaj 2 liczby\n";
-	int a, b;
+	float a, b;
 	cin >> a >> b;
-	int suma = a + b;
-	cout << a << " + " << b << " = " << suma << "\n";
+	float wynik = a * b;
+	cout << fixed << setprecision(2);
+	if (b >= 0) {
+		cout << a << " * " << b << " = " << wynik << "\n";
+	}
+	else {
+		cout << a << " * (" << b << ") = " << wynik << "\n";
+	}
+
 }
